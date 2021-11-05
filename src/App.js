a4ed2a1f01046694270a77badbb39cb0a98b324e
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-05 03:55:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from "react";
 
 function App() {
+  cont []
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+          </div>
+          <ul>
+            <li>
+              <strong>Views: 4000</strong>
+            </li>
+            <li>
+              <strong>Downloads: 300</strong>
+            </li>
+            <li>
+              <strong>Likes: 400</strong>
+            </li>
+          </ul>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-ful px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
+            #tag1
+          </span>
+          <span className="inline-block bg-gray-200 rounded-ful px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
+            #tag2
+          </span>
+          <span className="inline-block bg-gray-200 rounded-ful px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
+            #tag3
+          </span>
+        </div>
     </div>
   );
 }
